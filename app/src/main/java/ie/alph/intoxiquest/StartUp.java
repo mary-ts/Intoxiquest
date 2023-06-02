1d307d4b037d1ef250c7a26704d018486848dc91
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2023-06-03 00:00:20 +0100

    Added actual files for the startup screen instead of in main activity

---
+++ app/src/main/java/ie/alph/intoxiquest/StartUp.java
@@ -0,0 +1,25 @@
+package ie.alph.intoxiquest;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StartUp extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup);
+    }
+
+    public void OnClickSignIn(View view) {
+
+    }
+}
